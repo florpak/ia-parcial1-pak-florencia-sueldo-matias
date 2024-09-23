0099a069a9ec191e1e498922fc28e7fac5e395fe
MatiasSueldo <mes2008ar@gmail.com>
2024-09-23 00:41:25 -0300

    cambios

--- Assets/Scripts/AgentBoidScript.cs
+++ Assets/Scripts/AgentBoidScript.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentBoidScript : MonoBehaviour
 {
-    [SerializeField] protected int speed;
     private Vector3 velocity;
     [SerializeField] protected GameObject target;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _maxForce;
+    [SerializeField] private float viewRadius;
+    [SerializeField] private GameObject enemy;
 
 
     // Start is called before the first frame update
@@ -18,15 +22,61 @@ public class AgentBoidScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FollowTarget(target);
+        Vector3 vec;
+        if (Vector3.Distance(transform.position, enemy.transform.position)<= viewRadius)
+        {
+             vec = Flee(enemy.transform.position);
+        }
+        else
+        {
+            vec = Arrive(target.transform.position);
+
+        }
+        AddForce(vec);
+
+        transform.position += velocity * Time.deltaTime;
+        Debug.DrawLine(transform.position, transform.position + velocity,Color.cyan);
+        transform.forward = velocity;
     }
 
-
-    private void FollowTarget(GameObject target)
+    private Vector3 Seek(Vector3 targetPos,float maxSpeed)
     {
-        Vector3 dir = target.transform.position - transform.position;
-        velocity = Vector3.ClampMagnitude(dir.normalized * speed * Time.deltaTime, speed);
-        Debug.DrawLine(new Vector3(0, 0, 0), Vector3.ClampMagnitude(target.transform.position, speed));
-        transform.position += velocity;
+        Vector3 vectorDeseado = targetPos - transform.position;
+        Debug.DrawLine(transform.position, targetPos);
+
+        vectorDeseado.Normalize();
+        vectorDeseado *= maxSpeed;
+
+        Vector3 steering = vectorDeseado - velocity;
+        steering = Vector3.ClampMagnitude(steering, _maxForce* Time.deltaTime);
+        Debug.DrawLine(transform.position, transform.position+steering, Color.red);
+
+        return steering;
+
     }
+
+    private Vector3 Arrive(Vector3 targetPos)
+    {
+        float dist = Vector3.Distance(transform.position, targetPos);
+        if(dist < viewRadius)
+        {
+            return Seek(targetPos, _maxSpeed);
+        }
+        else
+        {
+            return Seek(targetPos, _maxSpeed * (dist / viewRadius));
+        }
+    }
+
+    private Vector3 Flee(Vector3 targetPos)
+    {
+        return -Seek(targetPos,_maxSpeed);
+    }
+
+    private void AddForce(Vector3 force)
+    {
+        velocity = Vector3.ClampMagnitude(force + velocity, _maxSpeed);
+    }
+
+
 }
