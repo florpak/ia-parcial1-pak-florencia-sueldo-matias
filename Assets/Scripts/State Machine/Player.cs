f9f3fca4c21884fcec059a908d7e9a7d40e462a4
MatiasSueldo <mes2008ar@gmail.com>
2024-09-26 22:33:02 -0300

    Añado cambios

--- Assets/Scripts/State Machine/Player.cs
+++ Assets/Scripts/State Machine/Player.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.PackageManager;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
